4c39cd9ffdb2ffb1403ee9651522c0ae14f2c14f
MrKeviscool <danielandkev@outlook.com>
2024-06-19 21:42:33 +0930

    new file:   .vscode/settings.json 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@
 #include <SFML/Graphics.hpp>
 using namespace std;
 
+//#aa84bf #84bf88 #735648 #734857 #ad667f
+//(170, 132, 191) (132, 191, 136) (115, 86, 72) (115, 72, 87) 
+
+
 int getnumber(const char *askfor);
 
 const int defaultpxmax = 15;
@@ -11,9 +15,29 @@ const int defaultpxmax = 15;
 int main(int argc, char *argv[]){
     sf::Image imgtex;
     int width, height, pxmax = defaultpxmax, pixelcount = 0;
+    sf::Color cols[4] = {sf::Color(170, 132, 191), sf::Color(132, 191, 136), sf::Color(115, 86, 72), sf::Color(115, 72, 87)};
     string imgToOpen = "ref.png";
-    if(argc > 1)
-        imgToOpen = string(argv[1]);
+    for(int i = 1; i < argc; i++){
+        if(string(argv[i]) == "--setcol"){
+            int colpos = 0;
+            if(argc-(i+5) < 0){
+                cout << "not enough args";
+                return 0;
+            }
+            for(int coi = i+1; coi < 5+i; coi++){
+                bool hashash = false;
+                if(argv[coi][0] == '#'){
+                    cout << "\nhash detected\n";
+                    hashash = true;
+                }
+
+                //upto extracting pairs of hex and making it RGB's
+            }
+        }
+    }
+    
+    return 0;
+
     if(!imgtex.loadFromFile(imgToOpen))
         return -1;
     width = imgtex.getSize().x;
@@ -25,6 +49,11 @@ int main(int argc, char *argv[]){
         catch(invalid_argument){
             cout << "invalid arguments, enter a number for 2nd paramter\n";
             return -1;
+        }
+    }
+    for(int args = 1; args < argc; args++){
+        if(argv[args] == (char *)"--setcol"){
+            cout << "setcol!!\n";
         }
     }
     pxmax = (width*height) * pxmax/100;
